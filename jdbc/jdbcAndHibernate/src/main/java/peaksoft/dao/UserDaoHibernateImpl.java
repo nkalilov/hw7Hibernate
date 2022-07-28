27e76ed59436a1a575884cace951cea0d424cdde
Homework 9 <nkalilov@gmail.com>
2022-07-28 09:03:28 +0600

    Hibernate task finished

--- jdbc/jdbcAndHibernate/src/main/java/peaksoft/dao/UserDaoHibernateImpl.java
+++ jdbc/jdbcAndHibernate/src/main/java/peaksoft/dao/UserDaoHibernateImpl.java
@@ -1,42 +1,102 @@
 package peaksoft.dao;
 
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 import peaksoft.model.User;
 
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
 
-    public UserDaoHibernateImpl() {
+//    public UserDaoHibernateImpl() {
+//
+//    }
 
+    public static org.hibernate.SessionFactory buildSessionFactory() {
+        try{
+            return new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
+        }
+        catch (Throwable e) {
+            System.out.println("Session doesnt created...");
+            throw new ExceptionInInitializerError(e);
+        }
+    }
+    private static final SessionFactory sessionFactory = buildSessionFactory();
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
+    public static void shutDown() {
+        getSessionFactory().close();
     }
 
     @Override
     public void createUsersTable() {
-
-    }
+        Session session = UserDaoHibernateImpl.getSessionFactory().openSession();
+        session.beginTransaction();
+        session.createSQLQuery("create table if not exists users ( " +
+                " id serial not null ," +
+                " name  varchar(50) not null, " +
+                " lastName varchar(50) not null ," +
+                " age int  not null )").executeUpdate();
+        session.getTransaction().commit();
+        session.close();
+        }
 
     @Override
     public void dropUsersTable() {
-
+        Session session = UserDaoHibernateImpl.getSessionFactory().openSession();
+        session.beginTransaction();
+        session.createSQLQuery("drop table if exists users").executeUpdate();
+        session.getTransaction().commit();
+        session.close();
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-
+        Session session = UserDaoHibernateImpl.getSessionFactory().openSession();
+        session.beginTransaction();
+        User user = new User(name, lastName, age);
+        session.save(user);
+        session.getTransaction().commit();
+        session.close();
+        System.out.println("User " + user + " successfully added...");
     }
 
     @Override
     public void removeUserById(long id) {
-
+        Session session = UserDaoHibernateImpl.getSessionFactory().openSession();
+        session.beginTransaction();
+        User user = (User) session.get(User.class, id);
+        session.delete(user);
+        session.getTransaction().commit();
+        session.close();
+        System.out.println("User " + user + " successfully deleted...");
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        Session session = UserDaoHibernateImpl.getSessionFactory().openSession();
+        session.beginTransaction();
+//        List<User> userList = session.createQuery("from User").getResultList();
+        session.getTransaction().commit();
+        session.close();
+//        return userList;
+        return getAllUsers();
     }
 
     @Override
     public void cleanUsersTable() {
+        Session session = UserDaoHibernateImpl.getSessionFactory().openSession();
+        session.beginTransaction();
+        Query query = session.createQuery("delete User");
+        query.executeUpdate();
+        session.getTransaction().commit();
+        session.close();
+        System.out.println("Deleted successfully...");
 
     }
 }
--- jdbc/jdbcAndHibernate/src/main/java/peaksoft/service/UserServiceImpl.java
+++ jdbc/jdbcAndHibernate/src/main/java/peaksoft/service/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package peaksoft.service;
 
+import peaksoft.dao.UserDaoHibernateImpl;
 import peaksoft.dao.UserDaoJdbcImpl;
 import peaksoft.model.User;
 
@@ -7,29 +8,29 @@ import java.util.List;
 
 public class UserServiceImpl implements UserService {
 
-    UserDaoJdbcImpl userDaoJdbc = new UserDaoJdbcImpl();
+    UserDaoHibernateImpl userDaoHibernate = new UserDaoHibernateImpl();
 
     public void createUsersTable() {
-        userDaoJdbc.createUsersTable();
+        userDaoHibernate.createUsersTable();
     }
 
     public void dropUsersTable() {
-        userDaoJdbc.dropUsersTable();
+        userDaoHibernate.dropUsersTable();
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        userDaoJdbc.saveUser(name, lastName, age);
+        userDaoHibernate.saveUser(name, lastName, age);
     }
 
     public void removeUserById(long id) {
-        userDaoJdbc.removeUserById(id);
+        userDaoHibernate.removeUserById(id);
     }
 
     public List<User> getAllUsers() {
-        return userDaoJdbc.getAllUsers();
+        return userDaoHibernate.getAllUsers();
     }
 
     public void cleanUsersTable() {
-        userDaoJdbc.cleanUsersTable();
+        userDaoHibernate.cleanUsersTable();
     }
 }
